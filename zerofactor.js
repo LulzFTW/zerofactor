cea363c02c407b16400976869cd6a4bec61e97c6
lulz <lulz@lulz.org>
2017-10-02 14:42:06 -0400

    use browserify and add HOTP generator

---
+++ zerofactor.js
@@ -0,0 +1,32 @@
+var notp = require('notp');
+
+console.log('zerofactor init');
+
+function onLoad(result) {
+  var selector = `[data-device-index="${result.device}"] .passcode-label`;
+  var passcode = document.querySelector(selector);
+
+  var input = passcode.querySelector('.passcode-input');
+  var button = passcode.querySelector('.auth-button');
+
+  var counter = parseInt(result.counter);
+  var passcode = notp.hotp.gen(result.secret, {
+    counter: counter
+  });
+  browser.storage.sync.set({
+    counter: counter + 1
+  });
+  console.log(passcode);
+
+  input.value = passcode;
+  button.click();
+  input.value = '';
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+var getting = browser.storage.sync.get();
+getting.then(onLoad, onError);
+
