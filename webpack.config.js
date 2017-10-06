fda38d907e45a9262a1b2bde24212e3b675f49e1
lulz <lulz@lulz.org>
2017-10-05 20:38:56 -0400

    remove uglifyjs plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,7 @@
-const UglifyEsPlugin = require('uglify-es-webpack-plugin');
-
-const plugins = [
-  new UglifyEsPlugin()
-];
-
 module.exports = {
   entry: `${__dirname}/src/zerofactor.js`,
   output: {
       path: `${__dirname}/extension`,
       filename: "zerofactor.js",
-  },
-  plugins
+  }
 };
