fb1941f2e9727d6b4c126ef014b1f68441657be1
lulz <lulz@lulz.org>
2017-10-06 11:12:55 -0400

    use device name as identifier instead of volatile device id

--- src/zerofactor.js
+++ src/zerofactor.js
@@ -2,25 +2,50 @@ var notp = require('notp');
 
 console.log('zerofactor init');
 
-function onLoad(result) {
-  var selector = `[data-device-index="${result.device}"] .passcode-label`;
-  var passcode = document.querySelector(selector);
+function login(deviceName, secret, counter) {
+  try {
+    var deviceID = getDeviceID(deviceName);
+    var passcode = notp.hotp.gen(secret, {
+      counter
+    });
 
-  var input = passcode.querySelector('.passcode-input');
-  var button = passcode.querySelector('.auth-button');
+    sendInput(deviceID, passcode);
 
-  var counter = parseInt(result.counter);
-  var passcode = notp.hotp.gen(result.secret, {
-    counter: counter
-  });
-  browser.storage.local.set({
-    counter: counter + 1
-  });
-  console.log(passcode);
+    // increment HOTP counter
+    browser.storage.local.set({
+      counter: counter + 1
+    });
+  } catch (e) {
+    onError(e);
+  }
+}
+
+function getDeviceID(deviceName) {
+  var devices = document.querySelector('select[name="device"]');
+  for (var i = 0; i < devices.options.length; i++) {
+    var option = devices.options[i];
+    if (option.text == deviceName) {
+      return option.value;
+    }
+  }
+  throw new Error(`device ${deviceName} not found`);
+}
+
+function sendInput(deviceID, passcode) {
+  var selector = `[data-device-index="${deviceID}"] .passcode-label`;
+  var row = document.querySelector(selector);
+
+  var input = row.querySelector('.passcode-input');
+  var button = row.querySelector('.auth-button');
 
   input.value = passcode;
   button.click();
   input.value = '';
+}
+
+function onLoad(result) {
+  var counter = parseInt(result.counter)
+  login(result.device, result.secret, counter);
 }
 
 function onError(error) {
