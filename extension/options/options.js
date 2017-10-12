f68098036b35b340f56e5aea343d988719bafc96
lulz <lulz@lulz.org>
2017-10-12 05:00:18 -0400

    implement importing Duo QR codes

--- extension/options/options.js
+++ extension/options/options.js
@@ -21,16 +21,41 @@ function restoreOptions() {
     }
   }
 
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
-
   var getting = browser.storage.local.get();
   getting.then(setCurrentChoice, onError);
 }
 
+function importQR(e) {
+  e.preventDefault();
+
+  var parsedUrl = new URL(document.querySelector("#url").value);
+  var data = parsedUrl.searchParams.get('value').split('-');
+
+  var code = data[0];
+  var host = atob(data[1]);
+
+  fetch(`https://${host}/push/v2/activation/${code}`, {
+    method: 'post'
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    if (json.stat == "OK") {
+      var secret = json.response.hotp_secret;
+      document.querySelector("#secret").value = secret;
+      document.querySelector("#counter").value = 0;
+    } else {
+      onError(json.message || 'Invalid reponse');
+    }
+  });
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
-document.querySelector("form").addEventListener("reset", e => {
+document.querySelector("#config").addEventListener("submit", saveOptions);
+document.querySelector("#config").addEventListener("reset", e => {
     browser.storage.local.clear();
 });
+document.querySelector("#qr").addEventListener("submit", importQR);
